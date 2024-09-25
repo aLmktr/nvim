24c3fff3ecbdfae4dfd4a1cce99ef2b1af2f325a
almktr <mktr.acc@gmail.com>
2024-09-25 22:56:38 +0400

    feat: add autocmd to detecte python venv and attach it to lsp

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -15,3 +15,41 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		end
 	end,
 })
+
+local project_venv_cache = {}
+
+-- automatically run find_venv on FileType python, but only once per project
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python",
+	callback = function()
+		local root_dir = vim.fn.getcwd()
+
+		-- check if we've already set up the venv for this project
+		if project_venv_cache[root_dir] then
+			return
+		end
+
+		local venv_path = root_dir .. "/.venv/bin/python"
+		if vim.fn.executable(venv_path) == 1 then
+			-- Set Pyright pythonPath
+			require("lspconfig").pyright.setup({
+				settings = {
+					pyright = {
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							ignore = { "*" },
+						},
+						pythonPath = venv_path,
+					},
+				},
+			})
+			print("Using virtual environment: " .. venv_path)
+			-- cache the project path to avoid redundant checks
+			project_venv_cache[root_dir] = true
+		else
+			print("No .venv found in project root")
+		end
+	end,
+})
