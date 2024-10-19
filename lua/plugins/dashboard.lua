1d174cada2ea546b2a156d870f8065a6c633b30f
almktr <mktr.acc@gmail.com>
2024-10-20 01:07:22 +0400

    feat: update icons and shortcus in dashboard startup

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,24 +1,62 @@
-local dashboard_custom_header = [[
+return {
+	{
+		"nvimdev/dashboard-nvim",
+		opts = function()
+			local logo = [[
  ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗
  ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║
  ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║
  ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║
  ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║
  ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝
-]]
+            ]]
 
-return {
-	"nvimdev/dashboard-nvim",
-	event = "VimEnter",
-	config = function()
-		require("dashboard").setup({
-			config = {
-				header = vim.split(dashboard_custom_header, "\n"),
-			},
-			hide = {
-				statusline = false,
-			},
-		})
-	end,
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+			local opts = {
+				theme = "doom",
+				hide = {
+					statusline = false,
+				},
+				config = {
+					header = vim.split(logo, "\n"),
+					center = {
+						{
+							icon = " ",
+							desc = "Open Projects Directory   ",
+							key = "p",
+							action = "e ~/projects/",
+						},
+						{
+							icon = " ",
+							desc = "Edit Neovim Config       ",
+							key = "e",
+							action = "e ~/.config/nvim/init.lua",
+						},
+						{
+							icon = " ",
+							desc = "Find Files               ",
+							key = "f",
+							action = "Telescope find_files",
+						},
+						{
+							icon = " ",
+							desc = "Search in Files          ",
+							key = "s",
+							action = "Telescope live_grep",
+						},
+						{
+							icon = " ",
+							desc = "Quit Neovim             ",
+							key = "q",
+							action = function()
+								vim.api.nvim_input("<cmd>qa<cr>")
+							end,
+						},
+					},
+				},
+			}
+			return opts
+		end,
+	},
 }
