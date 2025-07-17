b0dad00d1fba72a12a859c5d5b17538135b1cd91
aLmktr <mktr.acc@gmail.com>
2025-07-17 22:55:44 +0400

    feat: add bufferline offsets for neo-tree and toggleterm filetypes

--- lua/plugins/buffer-line.lua
+++ lua/plugins/buffer-line.lua
@@ -2,4 +2,16 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			offsets = {
+				{
+					filetype = "neo-tree",
+				},
+				{
+					filetype = "toggleterm",
+				},
+			},
+		},
+	},
 }
